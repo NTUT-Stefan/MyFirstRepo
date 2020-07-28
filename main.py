9486e8a1d431b51df1ceebd7befa6ff7040d3d44
Vincent <vincent930093@gmail.com>
2020-07-28 18:54:51 +0800

    get data from google map api and save the data in txt

--- main.py
+++ main.py
@@ -3,13 +3,42 @@ import time
 import sys
 from Trip import Trip
 import Trip.CrudTrip as crud
-
+import googlemaps
+import json 
 
 
 def main():
-    with open('API_KEY','r') as f:
-        API_KEY = f.readline()
-    print(API_KEY)
+    # 這是textsearch
+    # gmaps = googlemaps.Client(key = 'AIzaSyCe0qz12Ciy0JniIDZQzww6zl0L77A-qGM')
+    # params = {
+    #         'query': 'restaurant in Taipei Xinyi',
+    #         'key': 'AIzaSyCe0qz12Ciy0JniIDZQzww6zl0L77A-qGM',
+    #     }
+    # url_part = 'text'
+    # url = "/maps/api/place/%ssearch/json" % url_part
+    # x = gmaps._request(url, params)
+    # print(json.dumps(x))
+    # f = open("places.txt", "a")
+    # f.write(json.dumps(x))
+    # f.close()
+
+    # 這是nearby search
+
+    gmaps = googlemaps.Client(key = 'AIzaSyCe0qz12Ciy0JniIDZQzww6zl0L77A-qGM')
+    params = {
+            'location': '25.033018,121.562801',
+            'radius':  '1000',
+            'key': 'AIzaSyCe0qz12Ciy0JniIDZQzww6zl0L77A-qGM',
+            'keyword': 'food',
+            'language': 'zh-TW'
+        }
+    url_part = 'nearby'
+    url = "/maps/api/place/%ssearch/json" % url_part
+    x = gmaps._request(url, params)
+    print(json.dumps(x))
+    f = open("places.txt", "a")
+    f.write(json.dumps(x))
+    f.close()
 
 if __name__ == "__main__":
     main()
