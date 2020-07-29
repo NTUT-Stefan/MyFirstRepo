2554cdddd319aaf09d2eddce0bba08dd2fd0570d
Stefan Liao <t107820005@ntut.org.tw>
2020-07-30 00:20:31 +0800

    add data request from places api

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ import time
 import sys
 from Trip import Trip
 import Trip.CrudTrip as crud
-
-
+import googlemaps
+import pprint
+import json
 
 def main():
     with open('API_KEY','r') as f:
         API_KEY = f.readline()
-    print(API_KEY)
+    gmaps = googlemaps.Client(key = API_KEY)
+    params = {'query':'tourist+spots+in+Taitung', 'key':API_KEY}
+    result = gmaps._request('/maps/api/place/textsearch/json', params)
+    print(json.dumps(result))
+    
 
 if __name__ == "__main__":
     main()
